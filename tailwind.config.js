e33baebf19822eb8a9675a42d9bc2c68600b9932
Bruno Gonzales <hellobrunogonzales@gmail.com>
2021-01-06 20:01:44 -0500

    Added Inter fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      body: ["Inter Regular"],
+      display: ["Inter Bold"],
+    },
     extend: {},
   },
   variants: {
